d88f5179566751b8b33eff3fdfd6c368d0afe5bc
Jimmy Hooker <james.hooker@fivetran.com>
2022-07-13 16:48:44 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# fivetran-connect-card-script-nodejs
+# Fivetran Connect Card Generation Script in Node.JS
 Easily generate connect cards from the comfort of your chair using NodeJS
+
+## How this works
+We're going to setup a really simple script that will generate a connect card URL for Google Analytics. Want to try others? Check out the config examples here: https://fivetran.com/docs/rest-api/connectors/config
+
+## Prepare
+- You'll need a Fivetran account and your key and secret that you can find in settings. Find out more in our [getting started guide](https://fivetran.com/docs/rest-api/getting-started).
+- Make sure you have node/npm installed: https://nodejs.org/en/
+- Setup a destination in Fivetran (ie Snowflake, BigQuery, etc): https://fivetran.com/docs/destinations
+
+## Setup
+- Create a `.env` file in the root of the project and add your key, secret, groupId in the below format:
+```
+API_KEY=
+API_SECRET=
+GROUP_ID=
+```
+
+Run `npm install` in the root to install the project dependencies.
+
+## Run the script
+Open the folder for this repository in your commandline tool of choice and run the script. If you downloaded the folder to your desktop on a Mac, it would look like this:
+- `cd ~/Desktop/fivetran-connect-card-script-nodejs`
+- `node index.js`
+
+You should see it return a URL. You can enter that URL into your browser window and you should see a valid Connect Card. 
+
+## Learn more about Connect Cards and Powered by Fivetran
+- [Fivetran Connect Cards](https://fivetran.com/docs/rest-api/connectors/connect-card)
+- [Powered by Fivetran QuickStart](https://fivetran.com/docs/getting-started/powered-by-fivetran)
+
+
+
---
+++ index.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
+const randomWords = require('random-words');
+
+
+// Begin Auth
+const key = process.env.API_KEY;
+const secret = process.env.API_SECRET;
+
+const token = `${key}:${secret}`;
+const base64Encoded = new Buffer.from(token).toString('base64');
+// End Auth
+
+// Begin Config
+const group = process.env.GROUP_ID;
+const schemaId = randomWords({ exactly: 2, join: '_' });
+const connectorType = 'google_analytics';
+// End Config
+
+const connectCardGen = async () => {
+    
+    // Create the Connector and return its ID
+    console.log('\x1b[36m%s\x1b[0m','Creating a Connector...');
+    const connectorId = await axios(
+        {
+            method: 'post',
+            url: 'https://api.fivetran.com/v1/connectors',
+            headers: { 'Authorization': 'Basic '+ base64Encoded },
+            data: {
+                "service": connectorType,
+                "group_id": group,
+                "paused": "true",
+                "run_setup_tests": "false",
+                "config": {
+                    "schema": schemaId
+                }
+            }
+        }
+    )
+    .then(function (response) {
+        return response.data.data.id;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+    // Create the Connector Card Token and return it
+    console.log('\x1b[36m%s\x1b[0m','Creating a Connect Card Token...\n');
+    const connectCardToken = await axios(
+    {
+        method: 'post',
+        url: `https://api.fivetran.com/v1/connectors/${connectorId}/connect-card-token`,
+        headers: { 'Authorization': 'Basic '+ base64Encoded }
+    })
+    .then(function (response) {
+        return response.data.token;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+     // Construct the URL for a connect card and log it to the CLI
+    console.log('\x1b[36m%s\x1b[0m','All done! Get your URL below:');
+    console.log(`https://fivetran.com/connect-card/setup?redirect_uri=https://fivetran.com&auth=${connectCardToken}`)
+};
+
+connectCardGen();
