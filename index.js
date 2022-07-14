e950e246041e1965d8ddf6145a5664f76c98db87
Jimmy Hooker <james.hooker@fivetran.com>
2022-07-13 19:20:40 -0700

    Cleaned up async await with try catch blocks

--- index.js
+++ index.js
@@ -18,46 +18,48 @@ const connectorType = 'google_analytics';
 // End Config
 
 const connectCardGen = async () => {
+    let connectorId;
+    let connectCardToken;
     
     // Create the Connector and return its ID
     console.log('\x1b[36m%s\x1b[0m','Creating a Connector...');
-    const connectorId = await axios(
-        {
-            method: 'post',
-            url: 'https://api.fivetran.com/v1/connectors',
-            headers: { 'Authorization': 'Basic '+ base64Encoded },
-            data: {
-                "service": connectorType,
-                "group_id": group,
-                "paused": "true",
-                "run_setup_tests": "false",
-                "config": {
-                    "schema": schemaId
+    try {
+        const connectorData = await axios(
+            {
+                method: 'post',
+                url: 'https://api.fivetran.com/v1/connectors',
+                headers: { 'Authorization': 'Basic '+ base64Encoded },
+                data: {
+                    "service": connectorType,
+                    "group_id": group,
+                    "paused": "true",
+                    "run_setup_tests": "false",
+                    "config": {
+                        "schema": schemaId
+                    }
                 }
             }
-        }
-    )
-    .then(function (response) {
-        return response.data.data.id;
-    })
-    .catch(function (error) {
+        );
+        connectorId = connectorData.data.data.id;
+    } catch (error) {
         console.log(error);
-    });
+    }
 
     // Create the Connector Card Token and return it
     console.log('\x1b[36m%s\x1b[0m','Creating a Connect Card Token...\n');
-    const connectCardToken = await axios(
-    {
-        method: 'post',
-        url: `https://api.fivetran.com/v1/connectors/${connectorId}/connect-card-token`,
-        headers: { 'Authorization': 'Basic '+ base64Encoded }
-    })
-    .then(function (response) {
-        return response.data.token;
-    })
-    .catch(function (error) {
+    try {
+        const connectCardTokenData = await axios(
+            {
+                method: 'post',
+                url: `https://api.fivetran.com/v1/connectors/${connectorId}/connect-card-token`,
+                headers: { 'Authorization': 'Basic '+ base64Encoded }
+            }
+        );
+
+        connectCardToken = connectCardTokenData.data.token;
+    } catch (error) {
         console.log(error);
-    });
+    }
 
      // Construct the URL for a connect card and log it to the CLI
     console.log('\x1b[36m%s\x1b[0m','All done! Get your URL below:');
